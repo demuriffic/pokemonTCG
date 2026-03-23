7ad1e8ecd0f5701e704ae40021c89a7e30a501aa
demuriffic <cjr.gfx@gmail.com>
2026-03-23 17:12:29 +0800

    added api key support

--- src/services/pokemonTcgApi.js
+++ src/services/pokemonTcgApi.js
@@ -2,7 +2,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const pokemonTcgApi = createApi({
   reducerPath: 'pokemonTcgApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.pokemontcg.io/v2/' }),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: 'https://api.pokemontcg.io/v2/',
+    prepareHeaders: (headers) => {
+      const apiKey = import.meta.env.VITE_POKEMON_API_KEY;
+      if (apiKey) {
+        headers.set('X-Api-Key', apiKey);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getCards: builder.query({
       query: ({ page = 1, pageSize = 20, q = '' } = {}) => {
