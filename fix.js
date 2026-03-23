dfd246f52c42d662370a7e58e7b5c8bdfa2adf2a
Carlos Jiro G. Reaño <131967352+demuriffic@users.noreply.github.com>
2026-03-23 17:02:20 +0800

    Delete fix.js

--- fix.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-fs.writeFileSync('src/index.css', `@import "tailwindcss";\nbody { background-color: #f3f4f6; }\n`, 'utf8');
-fs.writeFileSync('src/App.css', '', 'utf8');
-fs.writeFileSync('src/main.jsx', fs.readFileSync('src/main.jsx', 'utf8').toString().replace(/^\uFEFF/, ''), 'utf8');
